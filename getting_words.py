dd93070b1129679d49244a451d22f60b6aee2239
Abhishek Agrawal <aaagrawal.abhishek@gmail.com>
2024-04-06 12:59:15 -0700

    Added Function endswith_ait which specifically checks that condition

--- getting_words.py
+++ getting_words.py
@@ -5,21 +5,26 @@ url = "https://www.wordgamedictionary.com/word-lists/5-letter-words/get.json"
 # Send a GET request to the URL
 response = requests.get(url)
 
+
+#Function to check words ending with ait
+def endswith_ait(data):
+    
+    # Print the list of 5-letter words
+        arr = []
+        for word in data:
+            if word["word"].endswith("ait"):
+                arr.append(word["word"])
+
+        return arr
+            
 # Check if the request was successful
 
 if response.status_code == 200:
     # Parse the JSON data
     data = response.json()
-    #print(data)
+    
+    print(endswith_ait(data))
 
     
-    # Print the list of 5-letter words
-    arr = []
-    for word in data:
-        if word["word"].endswith("ait"):
-            arr.append(word["word"])
-
-    print(arr)
-        #print(word["word"])
 else:
     print(f"Error: {response.status_code}")
